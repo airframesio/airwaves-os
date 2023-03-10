510032bfbf2ad8240c1be03d7abe23f48ad2b11c
Lane Jennison <lane@lane-fu.com>
2023-03-10 21:19:07 +0000

    quick fixes to adjust to armbian-next flavor of builder as its now the main branch

--- armbian/armbian-build.sh
+++ armbian/armbian-build.sh
@@ -14,16 +14,23 @@ cp -aR base/* armbian-build/userpatches/overlay/
 cp -a base/lib.config armbian-build/userpatches/
 cp -aR patches/* armbian-build/userpatches/
 
+##TODO refactor customize-image.sh to use the extension and hooks model with a userpatches/config-example.conf 
 cp -a base/customize-image.sh armbian-build/userpatches/
+##TODO rename this airwaveos  default is no longer loaded by default.
 cp -a base/config-default.conf armbian-build/userpatches/
+
+#if you want to override these variables, set them up front...   ex.  BOARD=whatever SHOW_DEBUG=yes ./armbian-build.sh
 
 BOARD=${BOARD:-rpi4b}
 BRANCH=${BRANCH:-current}
 RELEASE=${RELEASE:-jammy}
 EXPERT=${EXPERT:-no}
 PROGRESS_LOG_TO_FILE=${PROGRESS_LOG_TO_FILE:-yes}
+SHOW_DEBUG=${SHOW_DEBUG:-no}
+SHOW_COMMANDS=${SHOW_COMMANDS:-no}
+TEXT_IS_TOO_DARK=${TEXT_IS_TOO_DARK:-yes}  # if you have highcolor terminal you can set this to no... sometimes exta work needed in tmux etc
 
-BUILD_ARGS="docker BOARD=${BOARD} BUILD_MINIMAL=yes BUILD_DESKTOP=no BRANCH=${BRANCH} RELEASE=${RELEASE} WIREGUARD=no EXTRAWIFI=no PROGRESS_LOG_TO_FILE=${PROGRESS_LOG_TO_FILE}"
-
-time armbian-build/compile.sh ${BUILD_ARGS}
-
+BUILD_ARGS="TEXT_IS_TOO_DARK=${TEXT_IS_TOO_DARK} SHOW_DEBUG=${SHOW_DEBUG} SHOW_COMMANDS=${SHOW_COMMANDS} BOARD=${BOARD} BRANCH=${BRANCH} RELEASE=${RELEASE} WIREGUARD=no EXTRAWIFI=no PROGRESS_LOG_TO_FILE=${PROGRESS_LOG_TO_FILE} default"
+pushd ./armbian-build
+./compile.sh ${BUILD_ARGS}
+popd
