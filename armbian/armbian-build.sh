bab75ff0ade48421cdd58f5d9c6d24d76844de21
Lane Jennison <lane@lane-fu.com>
2023-05-28 16:49:53 +0000

    renames

--- armbian/armbian-build.sh
+++ armbian/armbian-build.sh
@@ -15,11 +15,11 @@ cp -ar extensions/* armbian-build/userpatches/extensions/
 cp -ar base/*.conf armbian-build/userpatches/
 
 ##FIXME should should store build.config in the extensions direction but wanted to sanity check with kevin first
-cp -a base/build.config armbian-build/userpatches/extensions/airwave-os/config/
+cp -a base/build.config armbian-build/userpatches/extensions/airwaves-os/config/
 
 TEXT_IS_TOO_DARK=${TEXT_IS_TOO_DARK:-yes}  # if you have highcolor terminal you can set this to no... sometimes exta work needed in tmux etc
 
-BUILD_ARGS="TEXT_IS_TOO_DARK=${TEXT_IS_TOO_DARK} airwave-os ${@}"
+BUILD_ARGS="TEXT_IS_TOO_DARK=${TEXT_IS_TOO_DARK} airwaves-os ${@}"
 pushd ./armbian-build
 ./compile.sh ${BUILD_ARGS}
 popd
