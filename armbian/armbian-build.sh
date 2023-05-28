f0380db16523409e2d0bf4a11b161e09760cb418
Lane Jennison <lane@lane-fu.com>
2023-05-28 10:57:38 +0000

    remove lib.config

--- armbian/armbian-build.sh
+++ armbian/armbian-build.sh
@@ -11,7 +11,6 @@ fi
 mkdir -p armbian-build/output/
 mkdir -p armbian-build/userpatches/overlay
 cp -aR base/* armbian-build/userpatches/overlay/
-cp -a base/lib.config armbian-build/userpatches/
 cp -aR patches/* armbian-build/userpatches/
 
 ##TODO refactor customize-image.sh to use the extension and hooks model with a userpatches/config-example.conf 
@@ -30,6 +29,7 @@ SHOW_DEBUG=${SHOW_DEBUG:-no}
 SHOW_COMMANDS=${SHOW_COMMANDS:-no}
 TEXT_IS_TOO_DARK=${TEXT_IS_TOO_DARK:-yes}  # if you have highcolor terminal you can set this to no... sometimes exta work needed in tmux etc
 
+##TODO add ${@} expansion to forward build args from launch of this script to compile
 BUILD_ARGS="TEXT_IS_TOO_DARK=${TEXT_IS_TOO_DARK} SHOW_DEBUG=${SHOW_DEBUG} SHOW_COMMANDS=${SHOW_COMMANDS} BOARD=${BOARD} BRANCH=${BRANCH} RELEASE=${RELEASE} WIREGUARD=no EXTRAWIFI=no PROGRESS_LOG_TO_FILE=${PROGRESS_LOG_TO_FILE} default"
 pushd ./armbian-build
 ./compile.sh ${BUILD_ARGS}
