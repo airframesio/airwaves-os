0ce56686cab4d9c3a8cf18d13815cafbbe1a5064
Lane Jennison <lane@lane-fu.com>
2023-05-28 15:54:24 +0000

    WIP checkpoint tailscale to extension

--- armbian/extensions/airwave-os/airwave-os.sh
+++ armbian/extensions/airwave-os/airwave-os.sh
@@ -1,114 +1,74 @@
 #!/bin/bash
 
-function user_config__airwave_os_extra_packages() {
-  display_alert "Add additional debian packages for airwaveOS depdencncies" "custom_airwave" "info"
+function extension_prepare_config__airwaves_os() {
+  display_alert "Target image will have AirwavesOS preinstalled" "${EXTENSION}" "info"
+}
+
+function user_config__airwaves_os_extra_packages() {
+  display_alert "Add additional debian packages for AirwaveOS dependencies" "airwaves_os" "info"
   add_packages_to_image armbian-config git golang avahi-daemon avahi-utils nala sudo
 }
 	
-CustomizeArmbian() {
-  echo "Running AROS customization installer..."
+function pre_install_kernel_debs__add_aros_scripts() {
+  display_alert "Copying airwaves-os scripts to image->/opt/aros" "airwaves_os" "info"
 
-  mkdir -p /opt/aros
-  cp -aR /tmp/overlay/* /opt/aros
-  chmod -R +x /opt/aros/scripts
+  mkdir -p "${SDCARD}"/opt/aros
+  cp -aR "${EXTENSION_DIR}"/* "${SDCARD}"/opt/aros
+  chmod -R +x "${SDCARD}"/opt/aros/scripts
 
-#  bash /tmp/overlay/customize-install-airwaves-os.sh $RELEASE $LINUXFAMILY $BOARD $BUILD_DESKTOP
 }
 
-. /opt/aros/build.config
+function post_family_tweaks__install_airwaves_os_base() {
 
-ImportFile() {
-  echo "Importing file"
-}
+  display_alert "Installing AirwaveOS base" "airwaves_os" "info"
 
-GenerateConfig() {
-  echo "Generating config"
-}
+  rm "${SDCARD}"/root/.not_logged_in_yet
 
-InstallAROS() {
+#  export LANG=C LC_ALL="en_US.UTF-8"
+#  export DEBIAN_FRONTEND=noninteractive
+#  export APT_LISTCHANGES_FRONTEND=none
 
-  echo ""
-  cat << EOF
-    _    _                                      ___  ____  
-   / \  (_)_ ____      ____ ___   _____  ___   / _ \/ ___| 
-  / _ \ | | '__\ \ /\ / / _\` \ \ / / _ \/ __| | | | \___ \ 
- / ___ \| | |   \ V  V / (_| |\ V /  __/\__ \ | |_| |___) |
-/_/   \_\_|_|    \_/\_/ \__,_| \_/ \___||___/  \___/|____/ 
-   Airwaves OS v1.0.0
 
-   Board    : $BOARD
-   Family   : $LINUXFAMILY
-   Release  : $RELEASE
-   Hostname : $HOSTNAME
-EOF
-  echo ""
-  echo "  Installing AROS base"
+  echo "    - "${SDCARD}"/etc/update-motd.d/10-armbian-header"
+  chmod -x "${SDCARD}"/etc/update-motd.d/10-armbian-header
 
-  echo root:airframes | chpasswd
-  rm /root/.not_logged_in_yet
+  echo "    + "${SDCARD}"/etc/update-motd.d/15-aros-header"
+  cp "${EXTENSION_DIR}"/config/15-aros-header "${SDCARD}"/etc/update-motd.d/
+  chmod +x "${SDCARD}"/etc/update-motd.d/15-aros-header
 
-  export LANG=C LC_ALL="en_US.UTF-8"
-  export DEBIAN_FRONTEND=noninteractive
-  export APT_LISTCHANGES_FRONTEND=none
+  echo "    + "${SDCARD}"/etc/update-motd.d/50-aros-help"
+  cp "${EXTENSION_DIR}"/config/50-aros-help "${SDCARD}"/etc/update-motd.d/
+  chmod +x "${SDCARD}"/etc/update-motd.d/50-aros-help
 
-  echo "  Config:"
+  echo "    + "${SDCARD}"/etc/avahi/services/aros.service"
+  cp "${EXTENSION_DIR}"/config/templates/avahi-aros.service.template "${SDCARD}"/etc/avahi/services/aros.service
 
-  echo "    - /etc/update-motd.d/10-armbian-header"
-  chmod -x /etc/update-motd.d/10-armbian-header
+  echo "    + "${SDCARD}"/root/.bashrc"
+  cp "${EXTENSION_DIR}"/config/templates/bashrc-custom.template "${SDCARD}"/root/.bashrc
 
-  echo "    + /etc/update-motd.d/15-aros-header"
-  cp /tmp/overlay/config/15-aros-header /etc/update-motd.d/
-  chmod +x /etc/update-motd.d/15-aros-header
+  echo "    + "${SDCARD}"/etc/aros/config.json"
+  mkdir -p "${SDCARD}"/etc/aros
+  cp "${EXTENSION_DIR}"/config/templates/aros-config.json.template "${SDCARD}"/etc/aros/config.json
 
-  echo "    + /etc/update-motd.d/50-aros-help"
-  cp /tmp/overlay/config/50-aros-help /etc/update-motd.d/
-  chmod +x /etc/update-motd.d/50-aros-help
-
-  echo "    + /etc/avahi/services/aros.service"
-  cp /tmp/overlay/config/templates/avahi-aros.service.template /etc/avahi/services/aros.service
-
-  echo "    + /root/.bashrc"
-  cp /tmp/overlay/config/templates/bashrc-custom.template /root/.bashrc
-
-  echo "    + /etc/aros/config.json"
-  mkdir -p /etc/aros
-  cp /tmp/overlay/config/templates/aros-config.json.template /etc/aros/config.json
-
-  echo "    + /etc/systemd/system/aros-first-run.service"
-  cp /tmp/overlay/config/templates/systemd-aros-first-run.service.template /etc/systemd/system/aros-first-run.service
+  echo "    + "${SDCARD}"/etc/systemd/system/aros-first-run.service"
+  cp "${EXTENSION_DIR}"/config/templates/systemd-aros-first-run.service.template "${SDCARD}"/etc/systemd/system/aros-first-run.service
   systemctl --no-reload enable aros-first-run.service
 
-  echo "    + /etc/systemd/system/aros-manager.service"
-  cp /tmp/overlay/config/templates/systemd-aros-manager.service.template /etc/systemd/system/aros-manager.service
+  echo "    + "${SDCARD}"/etc/systemd/system/aros-manager.service"
+  cp "${EXTENSION_DIR}"/config/templates/systemd-aros-manager.service.template "${SDCARD}"/etc/systemd/system/aros-manager.service
   systemctl --no-reload enable aros-manager.service
 
-  echo "    + /opt/aros/.needs-first-run"
-  touch /opt/aros/.needs-first-run
+  echo "    + "${SDCARD}"/opt/aros/.needs-first-run"
+  touch "${SDCARD}"/opt/aros/.needs-first-run
 
   echo "    + setting hostname to ${HOSTNAME}"
-  /tmp/overlay/scripts/change-hostname.sh $HOSTNAME
+  "${EXTENSION_DIR}"/scripts/change-hostname.sh $HOSTNAME
 
   echo ""
 
   echo "  Components:"
-  InstallDocker
-  InstallTailscale
 
   chage -d 0 root
 
 }
 
-InstallDocker() {
-  echo "    * Installing Docker"
-  curl -fsSL https://get.docker.com | sh 2> /dev/null > /dev/null
-}
-
-InstallTailscale() {
-  echo "    * Installing Tailscale"
-  curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-  curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list >/dev/null
-  sudo apt-get -qq update > /dev/null
-  sudo apt-get -qq install tailscale > /dev/null
-}
-
-InstallAROS "$@"
